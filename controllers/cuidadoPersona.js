67c90939f0eb9892247aed19a682cb726fc7b632
bruno-dahua <dahuabruno@gmail.com>
2024-09-25 15:19:24 -0300

    actualizo errores 404 not found para busqueda por id y por calificacion

--- controllers/cuidadoPersona.js
+++ controllers/cuidadoPersona.js
@@ -21,26 +21,58 @@ const getCuidadores = (req = request, res = response) => {
 }
 
 const getCuidadoresPorId = (req = request, res = response) => {
-  const { idCuidador } = req.params
+  const { idCuidador } = req.params;
+
+  //Valido que se este pasando un numero por parametro
+  if (isNaN(idCuidador)) {
+    return res.status(400).json({
+      msg: 'Error',
+      data: 'El ID del cuidador debe ser un número válido'
+    });
+  }
 
   axios.get(`https://66e20943c831c8811b5703f6.mockapi.io/cuidadoresPersonas/${idCuidador}`)
     .then((response) => {
-      const { data } = response
+      const { data } = response;
+
+      // Valido si no se encontraron datos
+      if (!data) {
+        return res.status(404).json({
+          msg: 'Error',
+          data: 'Cuidador no encontrado'
+        });
+      }
+
       res.status(200).json({
         msg: 'Ok',
         data
-      })
+      });
     })
     .catch((error) => {
-      res.status(400).json({
-        msg: 'Error',
-        error
-      })
-    })
-}
+      if (error.response && error.response.status === 404) {
+        res.status(404).json({
+          msg: 'Error',
+          data: 'Cuidador no encontrado'
+        });
+      } else {
+        res.status(400).json({
+          msg: 'Error',
+          error: error.message
+        });
+      }
+    });
+};
 
 const getCuidadoresPorCalificacion = async (req = request, res = response) => {
   const { calificacion } = req.params
+
+  //Valido que se este pasando un numero correcto por parametro
+  if (isNaN(calificacion) || calificacion < 1 || calificacion > 100) {
+    return res.status(400).json({
+      msg: 'Error',
+      data: 'La calificacion del cuidador debe ser un número válido, entre 1 y 100'
+    });
+  }
 
   try {
     const response = await axios.get('https://66e20943c831c8811b5703f6.mockapi.io/cuidadoresPersonas')
@@ -60,7 +92,7 @@ const getCuidadoresPorCalificacion = async (req = request, res = response) => {
     })
   } catch (error) {
     res.status(500).json({
-      msg: 'Error al obtener la información',
+      msg: 'Error',
       error: error.message
     })
   }
