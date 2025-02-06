efa4cd76e2aaea02deeb01141e5ae95e8ee40b85
NicoValenciano <lordmacako@gmail.com>
2025-02-05 22:38:14 -0300

    configuracion CORS

--- routes/jardineria.js
+++ routes/jardineria.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const { getJardineros, getJardinero, getJardineroFiltro } = require('../controllers/jardineria')
 
 const router = express.Router()
+
+router.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET'], // Only allow GET methods as defined in your routes
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 router.get('/filtro', getJardineroFiltro)
 router.get('/:idJardinero', getJardinero)
