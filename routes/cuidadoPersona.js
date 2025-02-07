606b79ea3dadab1ad80dc6d734c25bbd2f25e6b2
bruno-dahua <dahuabruno@gmail.com>
2025-02-07 16:04:01 -0300

    configuracion CORS

--- routes/cuidadoPersona.js
+++ routes/cuidadoPersona.js
@@ -1,8 +1,17 @@
 const express = require('express')
-
+const cors = require('cors')
 const { getCuidadores, getCuidadoresPorId, getCuidadoresPorCalificacion } = require('../controllers/cuidadoPersona')
 
 const router = express.Router()
+
+router.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
+  credentials: true,
+  preflightContinue: true,
+  optionsSuccessStatus: 204
+}))
 
 router.get('/filtro', getCuidadoresPorCalificacion)
 router.get('/', getCuidadores)
