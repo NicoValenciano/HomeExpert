fb361c4c839e37d8273e88e79e1f7fdd509efe2c
Camila Piergentili <camali_01@hotmail.com>
2025-02-08 21:25:44 -0300

    agrege cors a routes de paseadores

--- routes/paseador.js
+++ routes/paseador.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const { getAllPaseadores, getPaseadorPorId, getPaseadorPerrosConFiltro } = require('../controllers/paseador')
 
 const rutas = express.Router()
+
+rutas.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
+  credentials: true,
+  preflightContinue: true,
+  optionsSuccessStatus: 204
+}))
 
 rutas.get('/filtro', getPaseadorPerrosConFiltro)
 rutas.get('/:idPaseador', getPaseadorPorId)
